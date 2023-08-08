e073675a01161455f162fc69057b1dc9943c8f47
GodwinEgo <godwinehile04@gmail.com>
2023-08-08 22:16:42 +0100

    ap

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5001;
+const MONGO_URL = process.env.MONGO_URL;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+/*=============     MONGODB CONNECT     ================*/
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log("Error connecting to database: ", error);
+  });
 
 /*==============    ROUTES    ==================*/
 app.get("/", (req, res) => {
