e7d2b04e106ad74c80ea03f881300d3cb779c128
GodwinEgo <godwinehile04@gmail.com>
2023-08-08 21:58:23 +0100

    app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+/* ==============   IMPORTS ================*/
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+
+const app = express();
+app.use(express.json());
