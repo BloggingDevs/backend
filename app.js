5f356f0eb428223b5d3e6cd1662fc1df867d1721
GodwinEgo <godwinehile04@gmail.com>
2023-08-08 22:03:40 +0100

    gitignore

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const port = process.env.PORT || 5001;
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({ message: "WELCOME TO OUR BLOG BACKEND" });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on ${port} `);
 });
