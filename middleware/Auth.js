bb2b1bc366c91631b395d91b238550e848519046
GodwinEgo <godwinehile04@gmail.com>
2023-08-09 21:56:58 +0100

    user

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,1 +1,13 @@
-const
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.secretKey);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Unauthorized" });
+    console.error(error);
+  }
+};
